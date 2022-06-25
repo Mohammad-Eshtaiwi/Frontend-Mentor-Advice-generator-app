054b52c1cdd0fe9c96b002ad93949ad6aaf1b4f4
LetMeGuideU <53796803+LetMeGuideU@users.noreply.github.com>
2022-06-25 18:10:07 +0300

    Frontend Mentor | Advice generator app

--- script.js
+++ script.js
@@ -5,17 +5,19 @@ const adviceContainer = document.querySelector(".body");
 const adviceIdSpan = document.querySelector("h1 span");
 
 getAdviceButton.addEventListener("click", getAdvice);
-// init 
+// init function
 getAdvice();
 
 function getAdvice() {
     // disable getAdviceButton
     getAdviceButton.disabled = true;
     loader.classList.add("show");
+    getAdviceButton.classList.add("hide");
     fetch("https://api.adviceslip.com/advice")
         .then(response => response.json())
         .then(data => {
             loader.classList.remove("show");
+            getAdviceButton.classList.remove("hide");
             getAdviceButton.disabled = false;
 
             const advice = data.slip.advice;
